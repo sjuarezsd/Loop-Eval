e9d456ab3046f9de5eb46ccff9173c07d2dd74fb
“Steven <sjuarez.work@gmail.com>
2024-06-26 22:16:48 +0000

    Completed asana-tests.spec.ts, working as expected

--- tests/asana-tests.spec.ts
+++ tests/asana-tests.spec.ts
@@ -12,25 +12,21 @@ test.describe('Asana Data-Driven Tests', () => {
       // Login to Asana
       await test.step('Login to Asana', async () => {
         await page.goto('https://app.asana.com/-/login');
-        await page.fill('input[name="email"]', 'ben+pose@workwithloop.com');
-        await page.fill('input[name="password"]', 'Password123');
-        await page.click('button[type="submit"]');
-        await page.waitForNavigation();
+        await page.fill('input[name="e"]', 'ben+pose@workwithloop.com');
+        await page.locator('"Continue"').click();
+        await page.fill('input[name="p"]', 'Password123');
+        await page.locator('"Log in"').click();
       });
 
       // Navigate to the project page
       await test.step('Navigate to the project page', async () => {
-        const [projectName, projectType] = data.leftNav.split(', ');
-        await page.click(`text="${projectName}"`);
-        await page.click(`text="${projectType}"`);
-        await page.waitForNavigation();
+        await page.click(`span.TypographyPresentation:has-text("${data.leftNav}")`);
       });
 
       // Verify the card is within the right column
       await test.step('Verify the card is within the right column', async () => {
-        const columnLocator = page.locator(`text="${data.column}"`);
-        await columnLocator.scrollIntoViewIfNeeded();
-        const cardLocator = columnLocator.locator(`text="${data.card_title}"`);
+        const columnLocator = page.locator(`div.BoardBody-columnDraggableItemWrapper.SortableList-sortableItemContainer:has-text("${data.column}")`);
+        const cardLocator = columnLocator.locator(`span.TypographyPresentation.TypographyPresentation--m.BoardCard-taskName:has-text("${data.card_title}")`);
         await expect(cardLocator).toBeVisible();
       });
     });
